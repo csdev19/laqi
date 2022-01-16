f7fa29cd152be06b7abe6cafdb9a1e6fda2c1cb0
cristiandev <cristiansotomayor.dev@gmail.com>
2022-01-15 22:33:33 -0500

    using chokidar to listen changes on files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ const Configuration = require('./configuration');
 const Server = require('./server');
 
 const configuration = new Configuration();
+const chokidar = require('chokidar');
 
 const execute = () => {
-  const data = configuration.loadData();
-  const server = new Server(configuration.port, configuration.ip, configuration.path);
-  server.defineEndpoints(data);
-  server.start();
+  try {
+
+    const server = new Server(configuration.port, configuration.ip, configuration.path);
+    const data = configuration.loadData();
+    server.defineEndpoints(data);
+    server.start();
+    chokidar.watch('./mock-data', {
+      ignored: /[\/\\]\./,
+      persistent: false,
+    }).on('change', (event, path) => {
+      server.end();
+      server.defineEndpoints(data);
+      server.start();
+    });
+
+  } catch (error) {
+    console.log('error', error)
+  }
 }
 
 module.exports = execute;
