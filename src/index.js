f09a988015911e533070f56e5414fb4f7de50705
cristiandev <cristiansotomayor.dev@gmail.com>
2022-01-19 21:55:55 -0500

    update start of the aplication

--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ const chokidar = require('chokidar');
 const execute = () => {
   try {
 
-    const server = new Server(configuration.port, configuration.ip, configuration.path);
-    const data = configuration.loadData();
-    server.defineEndpoints(data);
-    server.start();
+    let data = configuration.loadData();
+    const server = new Server(configuration.port, configuration.ip, configuration.path, data);
+    server.initialize(data);
     chokidar.watch('./mock-data', {
       ignored: /[\/\\]\./,
       persistent: false,
-    }).on('change', (event, path) => {
-      server.end();
-      server.defineEndpoints(data);
-      server.start();
+    }).on('change', async (event, path) => {
+      data = configuration.loadData();
+      server.initialize(data);
     });
 
   } catch (error) {
